368ddcb1e8a86d37d53f44339213f16b707d6727
David <269518012+heavenlychatter@users.noreply.github.com>
2026-03-29 22:34:55 -0500

    fix: adds no prompt for non-interactive use

--- functions/appFunctions.py
+++ functions/appFunctions.py
@@ -1,18 +1,22 @@
 import logging
 import os
 
-def getAlphaAdvantageAPIKey():
+def getAlphaAdvantageAPIKey(no_prompt: bool = False) -> str:
     api_key = os.getenv("ALPHA_ADVANTAGE_API_KEY")
     if not api_key:
-        logging.error(
-            "ALPHA_ADVANTAGE_API_KEY environmental variable not found. Prompting..."
-        )
-        api_key = input("Please enter your AlphaAdvantage API key:")
-        if api_key:
-            os.putenv("ALPHA_ADVANTAGE_API_KEY", api_key)
-        else:
+        try:
+            if no_prompt:
+                raise ValueError("ALPHA_ADVANTAGE_API_KEY environment variable not found and no_prompt is set to True.")
             logging.error(
-                "No acceptable ALPHA_ADVANTAGE_API_KEY given. Please update the environmental variables and please try again."
+                "ALPHA_ADVANTAGE_API_KEY environmental variable not found. Prompting..."
             )
-
+            api_key = input("Please enter your AlphaAdvantage API key:")
+            if api_key:
+                os.putenv("ALPHA_ADVANTAGE_API_KEY", api_key)
+            else:
+                raise ValueError("No API key provided. Please set the ALPHA_ADVANTAGE_API_KEY environment variable or provide it when prompted.")
+        except Exception:
+            logging.exception("An error occurred while retrieving the AlphaAdvantage API key.")
+            return ""
+            
     return api_key
