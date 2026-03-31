184d896b248a354fce83daa2ed64020052e686cf
David <269518012+heavenlychatter@users.noreply.github.com>
2026-03-30 19:01:19 -0500

    feat: alphavantage request maker

--- functions/appFunctions.py
+++ functions/appFunctions.py
@@ -2,21 +2,21 @@ import logging
 import os
 
 def getAlphaAdvantageAPIKey(no_prompt: bool = False) -> str:
-    api_key = os.getenv("ALPHA_ADVANTAGE_API_KEY")
+    api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
     if not api_key:
         try:
             if no_prompt:
-                raise ValueError("ALPHA_ADVANTAGE_API_KEY environment variable not found and no_prompt is set to True.")
+                raise ValueError("ALPHA_VANTAGE_API_KEY environment variable not found and no_prompt is set to True.")
             logging.error(
-                "ALPHA_ADVANTAGE_API_KEY environmental variable not found. Prompting..."
+                "ALPHA_VANTAGE_API_KEY environmental variable not found. Prompting..."
             )
-            api_key = input("Please enter your AlphaAdvantage API key:")
+            api_key = input("Please enter your AlphaVantage API key:")
             if api_key:
-                os.putenv("ALPHA_ADVANTAGE_API_KEY", api_key)
+                os.putenv("ALPHA_VANTAGE_API_KEY", api_key)
             else:
-                raise ValueError("No API key provided. Please set the ALPHA_ADVANTAGE_API_KEY environment variable or provide it when prompted.")
+                raise ValueError("No API key provided. Please set the ALPHA_VANTAGE_API_KEY environment variable or provide it when prompted.")
         except Exception:
-            logging.exception("An error occurred while retrieving the AlphaAdvantage API key.")
+            logging.exception("An error occurred while retrieving the AlphaVantage API key.")
             return ""
             
     return api_key
--- functions/httpFunctions.py
+++ functions/httpFunctions.py
@@ -1,6 +1,29 @@
 import httpx
+from functions.appFunctions import getAlphaAdvantageAPIKey
 
 def httpClient() -> httpx.Client:
     client = httpx.Client()
-    
     return client
+    
+def makeAlphaVantageRequest(function: str, **kwargs) -> dict[str, any] | None: # this returns the raw data from the request as json, calling functions are responsible for parsing the data
+    base_url = "https://www.alphavantage.co/query"
+    params = {
+        "function": function,
+        "apikey": getAlphaAdvantageAPIKey()
+    }
+    params.update(kwargs) # pass parameters into this function as kwargs and they will be added to the params dict
+    
+    try:
+        with httpClient() as client:
+            response = client.get(base_url, params=params)
+            response.raise_for_status()
+            return response.json()
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e.response.status_code} - {e.response.text}")
+    except httpx.RequestError as e:
+        print(f"An error occurred while making the request: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+    
