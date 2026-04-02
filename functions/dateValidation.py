9921ab6d839ae9a23b9df57ccd70a1d5b7a37c62
Tim <t.timsaunders@gmail.com>
2026-04-02 17:26:29 -0500

    dateval outline

---
+++ functions/dateValidation.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+# function that verifies user input is a valid date format
+
+def verifyDateInput(string: input) -> bool:
+
+
+# function that verifies date input is included in dataset and can be graphed
+
